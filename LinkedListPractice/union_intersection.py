7e31b64945eba4934103e7c1541649fcce4b9c7e
ankitzin <ankit.zin279@gmail.com>
2022-11-05 01:20:55 +0530

    union Completed

--- LinkedListPractice/union_intersection.py
+++ LinkedListPractice/union_intersection.py
@@ -43,16 +43,58 @@ class LinkedList:
         return self.head_node
 
     def delete_head(self):
-        pass
+        if self.is_empty():
+            print("List is empty Nothing to delete")
+            return None
+        else:
+            if self.length() == 1:
+                print("deleted ")
+                self.head_node = None
+                return self.head_node
 
-    def delete(self):
-        pass
+        temp_node = self.get_head()
+        temp_node = temp_node.next_node
+        self.head_node = None
+        self.head_node = temp_node
+        return self.head_node
+
+    def delete(self, val):
+        deleted = False
+        prev_node = None
+        curr_node = self.head_node
+
+        if self.is_empty():
+            print("List is None")
+            return None
+
+        while curr_node:
+            if curr_node.data == val:
+                prev_node.next_node = curr_node.next_node
+                curr_node.next_node = None
+                deleted = True
+                break
+
+            prev_node = curr_node
+            curr_node = curr_node.next_node
+
+        return deleted
 
     def length(self):
-        pass
+        temp_node = self.head_node
+        len_ = 0
+        while temp_node:
+            temp_node = temp_node.next_node
+            len_ += 1
+
+        return len_
 
     def print_list(self):
-        pass
+        temp_node = self.get_head()
+        print(temp_node)
+        while temp_node is not None:
+            print(temp_node.data, end='->')
+            temp_node = temp_node.next_node
+        print(None)
 
     def insert_at_position(self):
         pass
@@ -61,11 +103,91 @@ class LinkedList:
         pass
 
     def remove_duplicates(self):
-        pass
+        prev_node = None
+        curr_node = self.get_head()
 
+        while curr_node:
+            prev_node = curr_node
+            while prev_node:
+                if prev_node.next_node:
+                    if curr_node.data == prev_node.next_node.data:
+                        new_next_node = prev_node.next_node.next_node
+                        prev_node.next_node = new_next_node
+                    else:
+                        prev_node = prev_node.next_node
+                else:
+                    prev_node = prev_node.next_node
+
+            curr_node = curr_node.next_node
+
+        return self.head_node
 
 ll = LinkedList()
 ll.insert_head(5)
 ll.insert_head(8)
 ll.insert_head(9)
+ll.insert_head(9)
 ll.insert_tail(90)
+print(ll.length())
+# ll.delete_head()
+# ll.delete(8)
+ll.remove_duplicates()
+ll.print_list()
+
+l2 = LinkedList()
+l2.insert_head(33)
+l2.insert_head(36)
+l2.insert_head(5)
+
+
+def remove_duplicates(lst: LinkedList):
+    if lst.is_empty():
+        return None
+
+    if lst.get_head().next_node is None:
+        return lst
+
+    first_node = lst.get_head()
+
+    while first_node:
+        sec_node = first_node
+        while sec_node:
+            if sec_node.next_node:
+                if first_node.data == sec_node.next_node.data:
+                    new_next_node = sec_node.next_node.next_node
+                    sec_node.next_node = new_next_node
+                else:
+                    sec_node = sec_node.next_node
+            else:
+                sec_node = sec_node.next_node
+
+        first_node = first_node.next_node
+
+    return lst
+
+
+def join_ll_union(l1:LinkedList, l2: LinkedList):
+    if l1.is_empty() and l2.is_empty():
+        return None
+    if l1.is_empty():
+        return l2
+    if l2.is_empty():
+        return l1
+
+    if not l1.is_empty():
+        curr_list = l1.head_node
+        next_list = l2.head_node
+        while curr_list:
+            curr_list = curr_list.next_node
+            if curr_list.next_node == None:
+                curr_list.next_node = next_list
+                break
+        return l1
+
+
+fin = join_ll_union(ll, l2)
+remove_duplicates(fin)
+fin.print_list()
+print("lets see how it goes")
+
+
