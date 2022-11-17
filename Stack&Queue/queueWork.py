26f34d6286ebdb9741c56e83f2ca9a689a2a3207
ankitzin <ankit.zin279@gmail.com>
2022-11-17 13:13:05 +0530

    changing

---
+++ Stack&Queue/queueWork.py
@@ -0,0 +1,10 @@
+"""
+we can apply this using Array but the complexity for this is O(n) because after operation we need to shift the element.
+Applying Queue using DLL
+    enqueue(element)
+    dequeue()
+    is_empty()
+    front()
+    rear()
+"""
+
